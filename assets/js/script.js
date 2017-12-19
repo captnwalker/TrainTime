fe9f4a14bb069542ac2923496514967a56496803
Steve Walker <captnwalker@gmail.com>
2017-12-19 00:55:46 -0500

    building structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-$(document).ready(function(){
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB-yelf2rmsbf6YICYZBNEO1bntvA5WjS4",
-    authDomain: "traintime-eb285.firebaseapp.com",
-    databaseURL: "https://traintime-eb285.firebaseio.com",
-    projectId: "traintime-eb285",
-    storageBucket: "",
-    messagingSenderId: "298743406219"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB-yelf2rmsbf6YICYZBNEO1bntvA5WjS4",
+        authDomain: "traintime-eb285.firebaseapp.com",
+        databaseURL: "https://traintime-eb285.firebaseio.com",
+        projectId: "traintime-eb285",
+        storageBucket: "",
+        messagingSenderId: "298743406219"
+    };
+    firebase.initializeApp(config);
 
-  //Variable/s
-  var database = firebase.database();
+    //Variables
+    var database = firebase.database();
+
+    var freq = parseInt(freq);
+
+    //Current time
+    var currentTime = moment();
+    console.log("Local Time: " + moment().format('HH:mm'));
+
+    $('#currentTime').text(currentTime);
+
+});
